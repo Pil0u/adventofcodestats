367653ef106f13d5f1dab299d770151c505ef105
Jérémie <jeremie.bonal@gmail.com>
2020-12-18 13:43:24 +0100

    Added coloring

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib import colors
 
 st.title('Advent of Code - Stats')
 
-st.table([chr(i+97) for i in range(0,10)])
+percentages = [90,35,70,10,100]
+
+df = pd.DataFrame(
+        dict([('Day %d' % i, percentage) for i, percentage in enumerate(percentages, start=1)]),
+        index=[0],
+    )
+
+st.table(df.style.background_gradient(cmap='RdYlGn', axis=1))
